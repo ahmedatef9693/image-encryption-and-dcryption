48c49feb20f0ce4fb998da0bce11377e5c501ca2
ahmed atef <99121164+ahmedatef9693@users.noreply.github.com>
2022-07-18 02:59:34 +0200

    Add files via upload

---
+++ PROJECT_ALGO/PROJECT_ALGO/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace PROJECT_ALGO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(635, 457);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(643, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(639, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Intial Seed:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(657, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 19);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Tap Pos:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(643, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 46);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Open";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "10001111",
+            "1110001011",
+            "01101000010",
+            "011010000101001",
+            "01101000010100010000",
+            "10011100"});
+            this.comboBox1.Location = new System.Drawing.Point(737, 17);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(172, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "6",
+            "8",
+            "2",
+            "10",
+            "16",
+            "0"});
+            this.comboBox2.Location = new System.Drawing.Point(737, 45);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(45, 21);
+            this.comboBox2.TabIndex = 10;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(936, 456);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ PROJECT_ALGO/PROJECT_ALGO/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace PROJECT_ALGO
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog ofd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static byte ctd(string bin)
+        {
+            byte val = 1;
+            byte sum = 0;
+            for (int i = bin.Length - 1; i >= 0; i--)
+            {
+                if (bin[i] == '1')
+                    sum += val;
+                val *= 2;
+            }
+
+            return sum;
+        }
+        public static string rem(string st1)
+        {
+            string ns = "";
+            for (int i = 1; i < st1.Length; i++)
+                ns += st1[i];
+            return ns;
+        }
+        public static string text;
+        public static int tappos;
+        public static string encrypt(string cl)
+        {
+            char res = '0';
+            int tap = cl.Length - tappos - 1;
+            // instead of 5 we must write (8-tappos-1).
+            for (int i = 0; i < 8; i++)
+            {
+                if (cl[tap] != cl[0])
+                    res = '1';
+
+                cl = rem(cl);
+                cl += res;
+                res = '0';
+            }
+            string nc = cl;
+            return nc;
+        }
+        public static byte encryptcolor(string text, string cl)
+        {
+            char res = '0';
+            byte final;
+            string newst = "";
+            int j = text.Length - 8;
+            for (int i = 0; i < 8; i++)
+            {
+                if (cl[i] != text[j])
+                    res = '1';
+                newst += res;
+                res = '0';
+                j++;
+            }
+            final = ctd(newst);
+            return final;
+        }     
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tappos = Convert.ToInt16(comboBox2.Text);
+            if (comboBox1.Text.Length < 8)
+                text = comboBox1.Text.PadLeft(8, '0');
+            else
+                text = comboBox1.Text;
+            if (tappos > comboBox1.Text.Length)
+            {
+                MessageBox.Show("Error tap pos!");
+            }
+            else
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Image);
+                int width = bmp.Width;
+                int height = bmp.Height;
+                Color p;
+                string n1;
+                string n2;
+                string n3;
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+
+                        p = bmp.GetPixel(x, y);
+                        byte a = p.A;
+                        byte b = p.B; string bst = Convert.ToString(b, 2).PadLeft(8, '0');
+                        byte g = p.G; string gst = Convert.ToString(g, 2).PadLeft(8, '0');
+                        byte r = p.R; string rst = Convert.ToString(r, 2).PadLeft(8, '0');
+                        n1 = encrypt(text);
+                        byte nr = encryptcolor(n1, rst);
+                        n2 = encrypt(n1);
+                        byte ng = encryptcolor(n2, gst);
+                        n3 = encrypt(n2);
+                        byte nb = encryptcolor(n3, bst);
+                        text = n3;
+                        bmp.SetPixel(x, y, Color.FromArgb(a, nr, ng, nb));
+                    }
+                }
+                pictureBox1.Image = bmp;
+                if (button1.Text == "Encrypt")
+                    button1.Text = "Decrypt";
+                else
+                    button1.Text = "Encrypt";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (ofd = new OpenFileDialog() { Filter = "All|*.*", ValidateNames = true, Multiselect = false })
+                {
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                        pictureBox1.Image = Image.FromFile(ofd.FileName);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
